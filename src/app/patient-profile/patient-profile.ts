88b01003aee3db1501162cb948e2b649192566cd
srucha20 <rucha.gsaraf@gmail.com>
2022-12-21 15:33:22 +0000

    Initial changes

---
+++ src/app/patient-profile/patient-profile.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'patient-profile',
+  templateUrl: 'patient-profile.html',
+  styleUrls: ['patient-profile.scss']
+})
+export class Tab1Page {
+
+  constructor() {}
+
+}
